83f087357a00f632b0a1c0198af8376b55131630
Kiana Long <kianajade42.kl@gmail.com>
2021-01-15 21:05:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,6 +50,8 @@ def won(board)
    board[win[0]] == board[win[1]] &&
      board[win[1]] == board[win[2]] &&
      position_taken?(board, win[0])
+   if draw puts false 
+   else puts true 
  end
 end
 def full?(board)
